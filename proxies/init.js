d0a3052a0373fa80db38b7fbe8b4ba44ebe39a73
QuackCode <quack.code.dk@gmail.com>
2019-02-11 00:32:27 +0100

    youtube proxies: now just return middleware function

--- proxies/init.js
+++ proxies/init.js
@@ -1,15 +1,14 @@
-module.exports = function init(proxyApp) {
-    const ytProxy = require("./proxy-main-youtube");
-    const ytSimpleProxy = require("./proxy-simple-youtube");
-    const {port, proxyPort} = require("../config");
+const ytProxy = require("./proxy-main-youtube");
+const ytSimpleProxy = require("./proxy-simple-youtube");
+const {port, proxyPort} = require("../config");
 // const harmonInjection = require("./harmon-injection");
 // proxyApp.use("/", harmonInjection);
-    proxyApp.use((req, res, next) => {
-        // console.log("url: ", req.url);
-        if (!req.url.startsWith("/yts"))
-            ytProxy.web(req, res);
-        else ytSimpleProxy.web(req, res);
-    });
 
+const proxyMiddlewareFn = (req, res, next) => {
+    // console.log("url: ", req.url);
+    if (!req.url.startsWith("/yts") || req.url.endsWith("base.js"))
+        ytProxy.web(req, res);
+    else ytSimpleProxy.web(req, res);
+}
 
-};
+module.exports = proxyMiddlewareFn;
